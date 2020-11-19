9c1dec011ec51d907281d298fb083abb7c6b21d7
kevin rodrigues <kevinrodrigues43@gmail.com>
2020-11-19 12:26:42 +0530

    updated

--- php/DBconnection.php
+++ php/DBconnection.php
@@ -1,18 +1,27 @@
 
 <?php
 
-$servername  = "shareddb-y.hosting.stackcp.net";
-$username= "usersDB-31353903d3";
-$password = "";
-$DBname = "usersDB-31353903d3";
+/* $host  = "localhost";
+$username= "root";
+$password = '';
+$DBname = "users"; */
+/* 
+$host  = "localhost";
+$username= "id15356956_kevin";
+$password = "57c4BSIRi4v!&_bA";
+$DBname = "id15356956_users"; */
 
-$connection = mysqli_connect($servername , $username, $password,$DBname);
+$host  = "shareddb-y.hosting.stackcp.net";
+$username= "usersDB-31353903d3";
+$password = "g61s0s3lrq"; 
+ $DBname = "usersDB-31353903d3"; 
+
+$connection = mysqli_connect($host , $username, $password ,$DBname);
 
 if(!$connection){
     die("Error in connecting to Database".mysqli_connect_error());
+
 }
 
-   
-
-
 ?>
+
